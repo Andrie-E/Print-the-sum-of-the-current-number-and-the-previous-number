ef882e2951bc4039eb1c6e2d36a86091550ea44d
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 14:34:00 +0800

    pseudocode

--- print_the_sum.py
+++ print_the_sum.py
@@ -1,1 +1,9 @@
-#Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+# Pseudocode
+
+# Provide the first number
+# Iterate the first 10 numbers
+# Solve  for the sum of the current and previous numbers
+# Print the sum
+# Update the current number
