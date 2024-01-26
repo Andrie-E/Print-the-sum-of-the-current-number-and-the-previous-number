0e1dab9a5443bec4ea0e6307e3e4b384b1557e09
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 14:44:29 +0800

    edited the print the sum code

--- print_the_sum.py
+++ print_the_sum.py
@@ -9,7 +9,7 @@ for i in range(10):
 # Solve  for the sum of the current and previous numbers
     current_sum = current_number + i
 # Print the sum
-    print(current_number, current_sum, i)
+    print(f"Current Number {current_number} Previous Number {i} Sum: {current_sum}")
 
 # Update the current number
     current_number = i
