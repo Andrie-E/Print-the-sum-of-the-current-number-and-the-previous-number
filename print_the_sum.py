876253bd6e4e09dc1a0b0b488d451a97b29f6e24
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 14:41:14 +0800

    code fo the pseudocode

--- print_the_sum.py
+++ print_the_sum.py
@@ -3,7 +3,14 @@
 # Pseudocode
 
 # Provide the first number
+current_number = 0
 # Iterate the first 10 numbers
+for i in range(10):
 # Solve  for the sum of the current and previous numbers
+    current_sum = current_number + i
 # Print the sum
+    print(current_number, current_sum, i)
+
 # Update the current number
+    current_number = i
+
